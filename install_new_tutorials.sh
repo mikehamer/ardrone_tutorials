63aaa989cf23ee91f576ec1fa5cca5f0e6555d81
Mike Hamer <mike@mikehamer.info>
2012-12-10 22:22:31 +0100

    Separated the script for upgrading from the script for installing
    This way the upgrade script will download an updated install script, which will then be run. Before, we would have needed to run the upgrade script twice, the first time to update the upgrade script, and the second time to run the updated upgrade script to install the new tutorials

---
+++ install_new_tutorials.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This file will be updated when new tutorials are available for download!
+
+echo "No new tutorials available at this time"
--- update_tutorials.sh
+++ update_tutorials.sh
@@ -2,6 +2,7 @@
 
 roscd
 
+# Update the AR.Drone Driver
 if [ -d "ardrone_autonomy" ];
 then
  cd ardrone_autonomy
@@ -15,6 +16,7 @@ else
  roscd
 fi
 
+# Update the first tutorial's files
 if [ -d "ardrone_tutorials_getting_started" ];
 then
  cd ardrone_tutorials_getting_started
@@ -22,6 +24,16 @@ then
  roscd
 else
  git clone https://github.com/mikehamer/ardrone_tutorials_getting_started.git
+fi
+
+# This will download an install file for new tutorials, which is then run.
+# We do it this way so that we can update the install file and run it straight away,
+# instead of having to run the update procedure twice (which we would need to do if this file were updated)
+if [ -f "ardrone_tutorials_getting_started/install_new_tutorials.sh" ];
+then
+ cd ardrone_tutorials_getting_started
+ source install_new_tutorials.sh
+ roscd
 fi
 
 rospack profile
