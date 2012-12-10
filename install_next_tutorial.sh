bc2d9f6e6da76e6509c8148d0551beb8013842e9
Mike Hamer <mike@mikehamer.info>
2012-12-10 14:12:49 +0100

    Moved the update tutorials script to the git so I can fix any problems without having to update the VM image

--- install_next_tutorial.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# When the next tutorial is released, this file will be updated to install required components
---
+++ update_tutorials.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+roscd
+
+if [-f ardrone_autonomy];
+then
+ cd ardrone_autonomy
+ git pull
+ roscd
+else
+ git clone https://github.com/AutonomyLab/ardrone_autonomy.git
+fi
+
+ardrone_autonomy/build_sdk.sh
+
+if [-f ardrone_tutorials_getting_started];
+then
+ cd ardrone_tutorials_getting_started
+ git pull
+ roscd
+else
+ git clone https://github.com/mikehamer/ardrone_tutorials_getting_started.git
+fi
+
+rospack profile
+rosstack profile
+
+rosmake -a
