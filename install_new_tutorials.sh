368ada7b6026c19d533c136fb2ed59230e1ef3a8
Mike Hamer <mike@mikehamer.info>
2012-12-30 16:14:47 +0100

    Updated install_new_tutorials script to install the second tutorial and also supplied an update which will be installed on ARDroneUbuntu machines

--- install_new_tutorials.sh
+++ install_new_tutorials.sh
@@ -2,4 +2,22 @@
 
 # This file will be updated when new tutorials are available for download!
 
-echo "No new tutorials available at this time"
+# first, a fix for the update_tutorials script found in the ARDroneUbuntu VM
+if [ -f "~/bin/update_tutorials" ];
+then
+ echo "Detected ARDroneUbuntu" 
+ rm ~/bin/update_tutorials
+ cp ARDroneUbuntu_Updates/update_tutorials ~/bin/update_tutorials
+ chmod +x ~/bin/update_tutorials
+fi
+
+roscd
+
+if [ -d "ardrone_tutorials_joystick_control" ];
+then
+ cd ardrone_tutorials_joystick_control
+ git pull
+ roscd
+else
+ git clone https://github.com/mikehamer/ardrone_tutorials_joystick_control.git
+fi
